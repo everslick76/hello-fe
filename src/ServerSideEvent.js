cbdf82297b4b0380e30274ff44a64b0f6881c774
Daniel Bengtsson <daniel.bengtsson@ingka.ikea.com>
2023-10-06 13:34:28 +0200

    chore: removed more unnecessary stuff

--- src/ServerSideEvent.js
+++ src/ServerSideEvent.js
@@ -3,44 +3,40 @@ import Button from "@mui/material/Button";
 import Snackbar from "@mui/material/Snackbar";
 import MuiAlert from "@mui/material/Alert";
 
-const Alert = React.forwardRef(function Alert(props, ref) {
-  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
-});
+const Alert = React.forwardRef((props, ref) => (
+  <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />
+));
 
-export function publish() {
-  const url = new URL("https://hello-app-n3tuxf5gqa-lz.a.run.app/publish");
-  return fetch(url).then((response) => response.json());
+export async function publish() {
+  const response = await fetch(
+    "https://hello-app-n3tuxf5gqa-lz.a.run.app/publish"
+  ).json();
+  return response.json();
 }
 
 export function ServerSideEvent() {
   const [result, setResult] = React.useState(false);
 
-  const handleClick = (event) => {
+  function handleClick(event) {
     publish(event.target.value).then(() => {
       setResult(true);
     });
-  };
+  }
 
-  const handleClose = () => {
+  function handleClose() {
     setResult(false);
-  };
+  }
 
   return (
-    <div className="App">
-      <div>
-        <Button variant="contained" onClick={handleClick}>
-          Save
-        </Button>
-        <Snackbar open={result} autoHideDuration={3000} onClose={handleClose}>
-          <Alert
-            onClose={handleClose}
-            severity="success"
-            sx={{ width: "100%" }}
-          >
-            Success
-          </Alert>
-        </Snackbar>
-      </div>
+    <div>
+      <Button variant="contained" onClick={handleClick}>
+        Save
+      </Button>
+      <Snackbar open={result} autoHideDuration={3000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success" sx={{ width: "100%" }}>
+          Success
+        </Alert>
+      </Snackbar>
     </div>
   );
 }
