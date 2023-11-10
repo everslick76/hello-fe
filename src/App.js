74f2d87c0261027e30376770a0e5e57d5a5a7090
Magnus Appelqvist <maapp4@ikea.com>
2023-11-10 11:05:20 +0100

    fixed promise

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import { ServerSideEvent } from "./ServerSideEvent";
 import SimpleBarChart from "./SimpleBarChart";
 import { useEffect, useState } from "react";
 
-export async function get() {
-  axios
+export function get() {
+  return new Promise((resolve) => {
+    axios
     .get("https://hello-app-n3tuxf5gqa-lz.a.run.app/chart")
     .then((response) => {
-      console.log(response.data);
-      return response.data;
+      const map = new Map(Object.entries(response.data));
+      resolve(map);
     })
     .catch((err) => {
       console.error(err);
     });
+  })
 }
 
 export function App() {
@@ -32,9 +34,9 @@ export function App() {
   }
 
   useEffect(() => {
+    setLoading(true)
     get()
-      .then(setLoading(true))
-      .then((names) => setTimeout(() => handleNames(names), 3000));
+      .then((a) => setTimeout(() => handleNames(a), 3000));
   }, []);
 
   return (
